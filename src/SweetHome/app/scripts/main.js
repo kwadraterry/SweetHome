c1b2c94e8769515647a91857efd5db4acdc7fab6
Екатерина Неповинных <kwadraterry@gmail.com>
2015-05-13 20:12:50 +0300

    Настроена работа js в шаблонах

--- src/SweetHome/app/scripts/main.js
+++ src/SweetHome/app/scripts/main.js
@@ -1,10 +1,4 @@
 define(function(require) {
 	'use strict';
-	var $ = require('jquery');
-	var helper = require('second');
-	var three = require('third');
-	three.one();
 	require('bxslider');
-	$('.main').bxSlider();
-	console.log(helper.name);
 });
