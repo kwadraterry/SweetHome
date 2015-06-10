23805a5b89cbbdb9d42ff51c8f4138dc8dee6795
Екатерина Неповинных <kwadraterry@gmail.com>
2015-06-10 10:56:46 +0300

    DB не работает

--- src/SweetHome/Models/DbInitializer.cs
+++ src/SweetHome/Models/DbInitializer.cs
@@ -8,10 +8,30 @@ namespace SweetHome.Models
 	class DbInitializer
 	{
 		private ApplicationDbContext _context;
-		private IEnumerable<ShelterAnimal> _animals = new List<ShelterAnimal>()
+		private static IList<Shelter> _shelters = new List<Shelter>()
 		{
-			new ShelterAnimal() {Name = "keesa", BirthDay = new DateTime(2010, 2, 15)},
-			new ShelterAnimal() {Name = "pyosya"}
+			new Shelter() {
+				Name = "Хочу Домой",
+				Address = "Челябинск",
+				Phone = "54321"
+			}
+		};
+		private static IEnumerable<ShelterAnimal> _animals = new List<ShelterAnimal>()
+		{
+			new ShelterAnimal() {
+				Name = "keesa",
+				BirthDay = new DateTime(2010, 2, 15),
+				AnimalType = AnimalType.Cat,
+				PlaceType = PlaceType.Shelter
+			},
+			new ShelterAnimal() {
+				Name = "pyosya",
+				BirthDay = new DateTime(2015, 2, 15),
+				AnimalType = AnimalType.Dog,
+				PlaceType = PlaceType.HomeShelter,
+				Color=Color.Dog,
+				Size=Size.Medium
+			},
 		};
 		public DbInitializer(ApplicationDbContext context)
 		{
@@ -19,16 +39,24 @@ namespace SweetHome.Models
 		}
 		public void InitializeData()
 		{
-			if (_context.Database.EnsureCreated())
-			{
-				CreateAnimals().Wait();
-			}
+			System.Console.WriteLine("Initializing data");
+			System.Console.WriteLine("Adding rows");
+			CreateShelters().Wait();
+			CreateAnimals().Wait();
 		}
 		private async Task CreateAnimals()
 		{
 			if (_context.ShelterAnimals.Count() == 0)
 			{
 				_context.ShelterAnimals.AddRange(_animals);
+				await _context.SaveChangesAsync();
+			}
+		}
+		private async Task CreateShelters()
+		{
+			if (_context.Shelters.Count() == 0)
+			{
+				_context.Shelters.AddRange(_shelters);
 				await _context.SaveChangesAsync();
 			}
 		}
