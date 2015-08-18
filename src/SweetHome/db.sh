f3bc0dadb8556d4c42165dc72c32778d3c93bc4a
kwadrat <ekaterina.nepovinnykh@redsolution.ru>
2015-08-18 18:49:05 +0300

    Добавлен файл на заполнение БД. Добавлены ссылки на приют на странице животных.

---
+++ src/SweetHome/db.sh
@@ -0,0 +1,4 @@
+dnx . db --command shelters --file shelters.txt
+dnx . db --command animals --file hochu_domoi_dogs.txt --shelter_id 1
+dnx . db --command animals --file luchik_nadezhdy.txt --shelter_id 2
+dnx . db --command animals --file ln_cats.txt --shelter_id 2
